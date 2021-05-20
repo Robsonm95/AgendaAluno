802ccc497c01d42973b79dcdf76b11971f9582dd
Robson Messias <robson.m95@hotmail.com>
2021-05-20 15:59:00 -0300

    Criação tela de listagem de alunos

--- app/src/main/java/com/robsonmm/alunoagenda/MainActivity.java
+++ app/src/main/java/com/robsonmm/alunoagenda/MainActivity.java
@@ -1,16 +1,30 @@
 package com.robsonmm.alunoagenda;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setTitle("Lista de Alunos");
         setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(
+            Arrays.asList("Robson","Maria","Thereza","Thereza","Thereza","Thereza","Thereza"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+       listaDeAlunos.setAdapter(new ArrayAdapter<String>(
+               this,
+               android.R.layout.simple_list_item_1,
+               alunos));
     }
+
 }
+
